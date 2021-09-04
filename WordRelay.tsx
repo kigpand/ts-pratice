4a545a3cc858f3e8db87c8cc930c5604d37f6c46
kigpand <kigpand@naver.com>
2021-09-04 22:26:54 +0900

    pratice-typescript-2 끝말잇기

---
+++ WordRelay.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { useState, useRef, useCallback } from 'react';
+
+const WordRelay = () =>{
+    const [word, setWord] = useState('지후니');
+    const [value, setValue] = useState<string>("");
+    const [result, setResult] = useState<string>("");
+    const inputEl = useRef<HTMLInputElement>(null);
+
+    const onSubmitForm = useCallback((e: React.FormEvent) =>{
+        e.preventDefault();
+        const input = inputEl.current;
+        if(word[word.length-1] === value[0]){
+            setResult('딩동댕');
+            setWord(value);
+            setValue('');
+            if(input){
+                input.focus();
+            }
+        }else{
+            setResult('땡');
+            setValue('');
+            if(input){
+                input.focus();
+            }
+        }
+    },[value]);
+
+    const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>)=>{
+        setValue(e.currentTarget.value);
+    },[]);
+
+    return(
+        <>
+        <div>{word}</div>
+        <form onSubmit={onSubmitForm}>
+            <input ref={inputEl}
+            value={value}
+            onChange={onChange}
+            />
+            <button>입력!</button>
+        </form>
+        <div>{result}</div>
+        </>
+    )
+};
+
+export default WordRelay;
